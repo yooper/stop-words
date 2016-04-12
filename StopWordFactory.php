0a04edfd9abe0ec4178643247ccc042751051e4f
yooper <dcardin2007@gmail.com>
2016-04-12 08:18:55 -0400

    Add stop words php factory

---
+++ StopWordFactory.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+* @author yooper(dcardin2007@gmail.com)
+* Loads a text file into memory
+* @usage  $stopWords = StopWordFactory::get('stop-words-fox.txt');
+*/
+ 
+class StopWordFactory
+{
+    /** 
+    *
+    * @returns array The array is empty if the stop words file does not exist
+    */
+    static public function get($fileName)
+    {
+	    $path = realpath(__DIR__.'/data/'.basename($fileName));
+        if(file_exists($path)) { 
+            return array_map('trim', file($path));
+        }
+        return [];
+    }
+}
